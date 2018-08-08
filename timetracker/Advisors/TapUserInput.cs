ba17dc49b53c6e9048964ed4cbbaad7e89e2ad6b
Ilia Alekseenko <yib000031c@aspire2student.ac.nz>
2018-08-08 16:38:55 +1200

    feat: mouse tapping

--- timetracker/Advisors/TapUserInput.cs
+++ timetracker/Advisors/TapUserInput.cs
@@ -13,18 +13,31 @@ namespace timetracker.Advisors
     class TapUserInput : ITimerAdvisor
     {
         private const int WH_KEYBOARD_LL = 13;
+        private const int WH_MOUSE_LL = 14;
         private const int WM_KEYDOWN = 0x0100;
-        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static LowLevelProc _proc = HookCallbackKeyBoard;
+        private static LowLevelProc _MouseProc = HookCallbackMouse;
         private static IntPtr _hookID = IntPtr.Zero;
-
+        private static IntPtr _MouseHookID = IntPtr.Zero;
+        
         private static DateTime LastKeyboardAction;
         private static DateTime LastMouseAction;
 
+        private enum MouseMessages
+        {
+            WM_LBUTTONDOWN = 0x0201,
+            WM_LBUTTONUP = 0x0202,
+            WM_MOUSEMOVE = 0x0200,
+            WM_MOUSEWHEEL = 0x020A,
+            WM_RBUTTONDOWN = 0x0204,
+            WM_RBUTTONUP = 0x0205
+        }
+        
         private bool TapMouse = false;
         private bool TapKeyboard = false;
 
         private int MaxKeyboardIdleInterval => Configuration.MaxKeyboardIdleInterval;
-        private int MaxMouseIdleInterval = 5;
+        private int MaxMouseIdleInterval = Configuration.MaxMouseIdleInterval;
 
         public TapUserInput(bool tapKeyboard, bool tapMouse)
         {
@@ -32,22 +45,32 @@ namespace timetracker.Advisors
             TapKeyboard = tapKeyboard;
         }
 
+
         public bool AdviseIfCanCount()
         {
             DateTime Now = DateTime.Now;
-            return TapKeyboard && LastKeyboardAction != null && Now.Subtract(LastKeyboardAction).TotalSeconds < MaxKeyboardIdleInterval;
+            bool _mouseYes = (!TapMouse || TapMouse && LastMouseAction != null && Now.Subtract(LastMouseAction).TotalSeconds < MaxMouseIdleInterval);
+            bool _kbYes = (!TapKeyboard || TapKeyboard && LastKeyboardAction != null && Now.Subtract(LastKeyboardAction).TotalSeconds < MaxKeyboardIdleInterval);
+            Console.WriteLine("Keyboard " + (_kbYes ? "YES" : "NO"));
+            Console.WriteLine("Mouse    " + (_mouseYes ? "YES" : "NO"));
+            return _mouseYes || _kbYes;
         }
 
         public void OnTimerStart()
         {
             if(TapKeyboard)
                 _hookID = SetKeyboardHook(_proc);
+
+            if (TapMouse)
+                _MouseHookID = SetMouseHook(_MouseProc);
         }
 
         public void OnTimerStop()
         {
             if (TapKeyboard)
                 UnhookWindowsHookEx(_hookID);
+            if (TapMouse)
+                UnhookWindowsHookEx(_MouseHookID);
         }
 
 
@@ -55,7 +78,7 @@ namespace timetracker.Advisors
         /*
          * Derived from https://stackoverflow.com/questions/604410/global-keyboard-capture-in-c-sharp-application
          */
-        private static IntPtr SetKeyboardHook(LowLevelKeyboardProc proc)
+        private static IntPtr SetKeyboardHook(LowLevelProc proc)
         {
             using (Process curProcess = Process.GetCurrentProcess())
             using (ProcessModule curModule = curProcess.MainModule)
@@ -65,9 +88,20 @@ namespace timetracker.Advisors
             }
         }
 
-        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+ 
+        private static IntPtr SetMouseHook (LowLevelProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_MOUSE_LL, proc,
+                GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
 
-        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        private delegate IntPtr LowLevelProc(int nCode, IntPtr wParam, IntPtr lParam);
+        
+        private static IntPtr HookCallbackKeyBoard(int nCode, IntPtr wParam, IntPtr lParam)
         {
             if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
             {
@@ -79,8 +113,58 @@ namespace timetracker.Advisors
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
         }
 
+        [StructLayout(LayoutKind.Sequential)]
+
+        private struct POINT
+
+        {
+
+            public int x;
+
+            public int y;
+
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MSLLHOOKSTRUCT
+
+        {
+
+            public POINT pt;
+
+            public uint mouseData;
+
+            public uint flags;
+
+            public uint time;
+
+            public IntPtr dwExtraInfo;
+
+        }
+
+        private static IntPtr HookCallbackMouse(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 &&
+                MouseMessages.WM_LBUTTONDOWN == (MouseMessages)wParam || MouseMessages.WM_MOUSEMOVE == (MouseMessages)wParam)
+            {
+                MSLLHOOKSTRUCT hookStruct = (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+                Console.WriteLine(hookStruct.pt.x + ", " + hookStruct.pt.y);
+                LastMouseAction = DateTime.Now;
+            }
+
+            return CallNextHookEx(_MouseHookID, nCode, wParam, lParam);
+
+        }
+
+
+
+      
+
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        //private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
--- timetracker/TimerDisplay.Designer.cs
+++ timetracker/TimerDisplay.Designer.cs
@@ -40,25 +40,28 @@
             this.lbTime.AutoSize = true;
             this.lbTime.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lbTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTime.Location = new System.Drawing.Point(12, 9);
+            this.lbTime.Location = new System.Drawing.Point(24, 17);
+            this.lbTime.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lbTime.Name = "lbTime";
-            this.lbTime.Size = new System.Drawing.Size(205, 54);
+            this.lbTime.Size = new System.Drawing.Size(409, 107);
             this.lbTime.TabIndex = 0;
             this.lbTime.Text = "00:00:00";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(70, 9);
+            this.label2.Location = new System.Drawing.Point(140, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.Size = new System.Drawing.Size(0, 25);
             this.label2.TabIndex = 1;
             // 
             // btStart
             // 
-            this.btStart.Location = new System.Drawing.Point(10, 66);
+            this.btStart.Location = new System.Drawing.Point(20, 127);
+            this.btStart.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btStart.Name = "btStart";
-            this.btStart.Size = new System.Drawing.Size(60, 27);
+            this.btStart.Size = new System.Drawing.Size(120, 52);
             this.btStart.TabIndex = 2;
             this.btStart.Text = "Start";
             this.btStart.UseVisualStyleBackColor = true;
@@ -67,9 +70,10 @@
             // btResume
             // 
             this.btResume.Enabled = false;
-            this.btResume.Location = new System.Drawing.Point(76, 66);
+            this.btResume.Location = new System.Drawing.Point(152, 127);
+            this.btResume.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btResume.Name = "btResume";
-            this.btResume.Size = new System.Drawing.Size(75, 27);
+            this.btResume.Size = new System.Drawing.Size(150, 52);
             this.btResume.TabIndex = 3;
             this.btResume.Text = "Resume";
             this.btResume.UseVisualStyleBackColor = true;
@@ -78,9 +82,10 @@
             // btStop
             // 
             this.btStop.Enabled = false;
-            this.btStop.Location = new System.Drawing.Point(157, 66);
+            this.btStop.Location = new System.Drawing.Point(314, 127);
+            this.btStop.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btStop.Name = "btStop";
-            this.btStop.Size = new System.Drawing.Size(60, 27);
+            this.btStop.Size = new System.Drawing.Size(120, 52);
             this.btStop.TabIndex = 4;
             this.btStop.Text = "Stop";
             this.btStop.UseVisualStyleBackColor = true;
@@ -88,18 +93,22 @@
             // 
             // TimerDisplay
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(233, 105);
+            this.ClientSize = new System.Drawing.Size(466, 202);
             this.ControlBox = false;
             this.Controls.Add(this.btStop);
             this.Controls.Add(this.btResume);
             this.Controls.Add(this.btStart);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lbTime);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "TimerDisplay";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "TimerDisplay";
             this.TopMost = true;
