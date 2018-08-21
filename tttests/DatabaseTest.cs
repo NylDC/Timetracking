ad5690461423227197167eecad783252d7b6df92
Sergey Shevchenko <yib00002nr@aspire2student.ac.nz>
2018-08-22 10:13:56 +1200

    test: added DatabaseTest for WhereCondition and WhereGroup

---
+++ tttests/DatabaseTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using timetracker.Services;
+
+namespace tttests
+{
+    [TestClass]
+    public class DatabaseTest
+    {
+        [TestMethod]
+        public void WhereCondition_AgtB()
+        {
+            WhereCondition wc = new WhereCondition() {
+                Left = "A",
+                Right = "B",
+                Operator = ">"
+            };
+            string correctValue = "[A] >'B'";
+            string testValue = wc.Build();
+            MyAssert.Equals(correctValue, testValue);
+        }
+
+        [TestMethod]
+        public void WhereCondition_AB()
+        {
+            WhereCondition wc = new WhereCondition()
+            {
+                Left = "A",
+                Right = "B"
+            };
+            string correctValue = "[A] ='B'";
+            string testValue = wc.Build();
+            MyAssert.Equals(correctValue, testValue);
+        }
+
+        [TestMethod]
+        public void WhereCondition_AisNULL()
+        {
+            WhereCondition wc = new WhereCondition()
+            {
+                Left = "A",
+                Operator = "IS",
+                Right = "NULL"
+            };
+            string correctValue = "[A] IS NULL";
+            string testValue = wc.Build();
+            MyAssert.Equals(correctValue, testValue);
+        }
+
+        [TestMethod]
+        public void WhereCondition_AisnotNULL()
+        {
+            WhereCondition wc = new WhereCondition()
+            {
+                Left = "A",
+                Operator = "IS NOT",
+                Right = "NULL"
+            };
+            string correctValue = "[A] IS NOT NULL";
+            string testValue = wc.Build();
+            MyAssert.Equals(correctValue, testValue);
+        }
+
+        [TestMethod]
+        public void WhereGroup_2AND()
+        {
+            WhereGroup wg = new WhereGroup() {
+                new WhereCondition("A","B"),
+                new WhereCondition("C","D"),
+            };
+           
+            string correctValue = "([A] ='B' AND [C] ='D')";
+            string testValue = wg.Build();
+            MyAssert.Equals(correctValue, testValue);
+        }
+
+        [TestMethod]
+        public void WhereGroup_2OR()
+        {
+            WhereGroup wg = new WhereGroup() {
+                new WhereCondition("A","B"),
+                new WhereCondition("C","D"),
+            };
+
+            string correctValue = "[A] ='B' OR [C] ='D'";
+            string testValue = wg.Build();
+            MyAssert.Equals(correctValue, testValue);
+        }
+    }
+}
