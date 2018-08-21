58078a57766b66ffd0c186ea121cadf9ff8cbc20
Sergey Shevchenko <yib00002nr@aspire2student.ac.nz>
2018-08-22 09:21:31 +1200

    feat: added "tttests" project with test cases

---
+++ tttests/UsersTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using timetracker.Services;
+using timetracker.Structs;
+
+namespace tttests
+{
+    [TestClass]
+    public class UsersTest
+    {
+        const string AuthUserName = "user";
+        const string AuthUserPass = "123456";
+
+        const string AuthAdminName = "admin";
+        const string AuthAdminPass = "123456";
+
+        [TestMethod]
+        public void LoginUserWithCorrectCredentials()
+        {
+            User user = Auth.Authenticate(AuthUserName, AuthUserPass);
+            Assert.IsNotNull(user);
+            Assert.AreEqual(user, Auth.CurrentUser);
+            Auth.Logout();
+            Assert.IsNull(Auth.CurrentUser);
+        }
+
+        [TestMethod]
+        public void LoginUserWithWrongCredentials()
+        {
+            Auth.Logout();
+            User user = Auth.Authenticate(AuthUserName, AuthUserPass+"wrong");
+            Assert.IsNull(user);
+        }
+
+        [TestMethod]
+        public void LoginAdminWithCorrectCredentials()
+        {
+            User user = Auth.Authenticate(AuthAdminName, AuthAdminPass);
+            Assert.IsNotNull(user);
+            Assert.AreEqual(user, Auth.CurrentUser);
+            Auth.Logout();
+            Assert.IsNull(Auth.CurrentUser);
+        }
+
+        [TestMethod]
+        public void LoginAdminWithWrongCredentials()
+        {
+            Auth.Logout();
+            User user = Auth.Authenticate(AuthAdminName, AuthAdminPass + "wrong");
+            Assert.IsNull(user);
+        }
+    }
+}
